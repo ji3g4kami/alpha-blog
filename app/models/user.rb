e241b29dde26413f04ae80eed83ec9dc5b36a1c6
Deng Li <ji3g4kami@gmail.com>
2017-09-07 10:25:40 +0800

    Create shoe action and add gravatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :article
+  has_many :articles
   before_save {self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
